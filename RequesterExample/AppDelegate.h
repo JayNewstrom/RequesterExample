fe80ee8b84f0b102dd11f57318019c27be08cd95
Jay Newstrom <jaynewstrom@Jays-MacBook-Pro.local>
2012-02-22 21:34:46 -0600

    Initial Commit

---
+++ RequesterExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RequesterExample
+//
+//  Created by Jay Newstrom on 2/22/12.
+//  Copyright (c) 2012 Jay's Computer Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RequesterExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RequesterExample
+//
+//  Created by Jay Newstrom on 2/22/12.
+//  Copyright (c) 2012 Jay's Computer Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
